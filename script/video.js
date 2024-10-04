7fb66aaf1d4350b4be284ed7e683b0a5e29a2dc2
nakib-abid <nakib.abid@gmail.com>
2024-10-04 15:39:48 +0600

    load videos

--- script/video.js
+++ script/video.js
@@ -3,24 +3,53 @@
 //  create display catagories function
 
 const loadCatagories = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayCatagories(data.categories))
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCatagories(data.categories))
     .catch((error) => console.log(error));
 };
 
-const displayCatagories = (catagories) => {
-    const categoryContainer = document.getElementById('categories');
-    catagories.forEach(item => {
-        // create a button
-        console.log(item)
-        const button = document.createElement('button');
-        button.classList = "btn";
-        button.innerText = item.category;
-        categoryContainer.append(button);
-    });
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
 };
 
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+         <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `;
+    videoContainer.append(card);
+  });
+};
+
+const displayCatagories = (catagories) => {
+  const categoryContainer = document.getElementById("categories");
+  catagories.forEach((item) => {
+    // create a button
+    // console.log(item)
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+    categoryContainer.append(button);
+  });
+};
 
 loadCatagories();
-displayCatagories();
+loadVideos();

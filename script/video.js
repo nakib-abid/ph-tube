7a45ab940a84b90bea2b1c5d4d2e5068c798621b
nakib-abid <nakib.abid@gmail.com>
2024-10-05 10:51:05 +0600

    added search button

--- script/video.js
+++ script/video.js
@@ -24,8 +24,8 @@ const loadCatagories = () => {
     .catch((error) => console.log(error));
 };
 
-const loadVideos = () => {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideos = (searchText = "") => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res) => res.json())
     .then((data) => displayVideos(data.videos))
     .catch((error) => console.log(error));
@@ -42,6 +42,28 @@ const loadCategoryVideos = (id) => {
       displayVideos(data.category);
     })
     .catch((error) => console.log(error));
+};
+
+const loadDetails = async (videoId) => {
+  const uri = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  const res = await fetch(uri);
+  const data = await res.json();
+  displayDetails(data.video);
+};
+
+const displayDetails = (video) => {
+  const detailsContainer = document.getElementById("modal-Content");
+
+  detailsContainer.innerHTML = `
+  <img src=${video.thumbnail} />
+  <p>${video.description}</p>
+  `;
+
+  // way - 1
+  // document.getElementById('showModalData').click();
+
+  // way - 2
+  document.getElementById("customModal").showModal();
 };
 
 const displayVideos = (videos) => {
@@ -93,9 +115,11 @@ const displayVideos = (videos) => {
           ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`
           : ""
       }
-     <div/>
+     </div>
      
-     <p></p>
+     <p> <button onclick="loadDetails('${
+       video.video_id
+     }')" class="btn btn-sm btn-success">Details</button> </p>
     </div>
   </div>
         `;
@@ -118,6 +142,10 @@ const displayCatagories = (catagories) => {
     categoryContainer.append(buttonContainer);
   });
 };
+
+document.getElementById('search-input').addEventListener('keyup', (e)=>{
+  loadVideos(e.target.value);
+});
 
 loadCatagories();
 loadVideos();
